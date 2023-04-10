fb3609d9ad8111f5479add791dd08ed717b9bc0d
Jacob <jacob.ilin.us@gmail.com>
2023-04-09 17:14:26 -0700

    Improved code readability and organization in Messages widget by refactoring and adding comments

--- chat/app/lib/message.dart
+++ chat/app/lib/message.dart
@@ -1,10 +1,20 @@
 import 'timestamp.dart';
 
+/// A class representing a chat message with a text, timestamp, user ID, and optional username and link.
 class Message {
+  /// The text content of the message.
   final String text;
+
+  /// The timestamp when the message was created.
   final Timestamp createdAt;
+
+  /// The user ID of the sender.
   final String userId;
+
+  /// The username of the sender (optional).
   final String? username;
+
+  /// A link associated with the message, if any.
   final String? link;
 
   Message({
@@ -14,4 +24,9 @@ class Message {
     required this.username,
     this.link,
   });
+
+  @override
+  String toString() {
+    return 'Message(text: $text, createdAt: $createdAt, userId: $userId, username: $username, link: $link)';
+  }
 }
--- chat/app/lib/messages.dart
+++ chat/app/lib/messages.dart
@@ -2,9 +2,11 @@ import 'message.dart';
 import 'message_bubble.dart';
 import 'package:flutter/material.dart';
 
+/// A class representing the Messages widget, which displays a list of MessageBubble widgets.
 class Messages extends StatefulWidget {
   final List<Message> _messages;
-  final ScrollController controller = new ScrollController();
+  final ScrollController controller = ScrollController();
+
   Messages(this._messages);
 
   @override
@@ -12,33 +14,50 @@ class Messages extends StatefulWidget {
 }
 
 class _MessagesState extends State<Messages> {
+  /// Replaces language prefixes in the message text.
+  String _processMessageText(String messageText) {
+    const List<String> languagePrefixes = [
+      'Italian: ',
+      'German: ',
+      'Portuguese: ',
+      'Dutch: ',
+      'Russian: ',
+      'American Spanish: ',
+      'Mexican Spanish: ',
+      'Canadian French: ',
+      'French: ',
+      'Spanish: ',
+      'American English: ',
+      'Australian English: ',
+      'British English: ',
+      'English: ',
+    ];
+
+    for (final prefix in languagePrefixes) {
+      messageText = messageText.replaceAll(prefix, '');
+    }
+
+    return messageText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       reverse: true,
       itemCount: widget._messages.length,
       controller: widget.controller,
-      itemBuilder: (ctx, index) => MessageBubble(
-        widget._messages[index].text
-            .replaceAll('Italian: ', '')
-            .replaceAll('German: ', '')
-            .replaceAll('Portuguese: ', '')
-            .replaceAll('Dutch: ', '')
-            .replaceAll('Russian: ', '')
-            .replaceAll('American Spanish: ', '')
-            .replaceAll('Mexican Spanish: ', '')
-            .replaceAll('Canadian French: ', '')
-            .replaceAll('French: ', '')
-            .replaceAll('Spanish: ', '')
-            .replaceAll('American English: ', '')
-            .replaceAll('Australian English: ', '')
-            .replaceAll('British English: ', '')
-            .replaceAll('English: ', ''),
-        widget._messages[index].username,
-        widget._messages[index].userId == 'Me',
-        widget._messages[index].link,
-        key: ValueKey(widget._messages[index].createdAt.toString()),
-      ),
+      itemBuilder: (ctx, index) {
+        final message = widget._messages[index];
+        final processedText = _processMessageText(message.text);
+
+        return MessageBubble(
+          processedText,
+          message.username,
+          message.userId == 'Me',
+          message.link,
+          key: ValueKey(message.createdAt.toString()),
+        );
+      },
     );
   }
 }
