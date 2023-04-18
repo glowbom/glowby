fdc63784ac4cb3572c42e6f40ddc8825655af618
Jacob <jacob.ilin.us@gmail.com>
2023-04-17 21:14:33 -0700

    adding importing glowbom files

--- app/lib/preset_talk.dart
+++ app/lib/preset_talk.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'color_utils.dart';
 import 'chat_screen.dart';
@@ -11,6 +13,7 @@ class _TalkState extends State<Talk> {
   String? _mainColor;
   bool? _voice = false;
   List<Map<String, Object>> _questions = [];
+  GlobalKey<ChatScreenState> chatScreenKey = GlobalKey();
 
   _TalkState(this._content);
 
@@ -44,6 +47,39 @@ class _TalkState extends State<Talk> {
             _questions = buildQuestions(_content['questions']);
             _pressed100();
           }));
+    }
+  }
+
+  void _startFilePicker() async {
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['glowbom'], // Limit the file extension to 'glowbom'
+      );
+
+      if (result != null) {
+        PlatformFile file = result.files.first;
+        Uint8List? fileBytes = file.bytes;
+        if (fileBytes != null) {
+          String content =
+              utf8.decode(fileBytes); // Decode the Uint8List to a String
+          Map<String, dynamic> fileContent = json.decode(content);
+
+          // Do something with the file content, e.g., update the questions
+          setState(() {
+            _content = fileContent;
+            _title = _content['title'];
+            _mainColor = _content['main_color'] ?? 'Blue';
+            _voice = _content['voice'] ?? false;
+            _questions = buildQuestions(_content['questions']);
+          });
+
+          print(_content);
+          print('name = ${_content['start_over']}');
+        }
+      }
+    } catch (e) {
+      print('Error: $e'); // Log the exception
     }
   }
 
@@ -110,31 +146,37 @@ class _TalkState extends State<Talk> {
                         : Colors.grey),
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              _title != null ? _title! : 'Chat App',
-              style: TextStyle(
-                color: Colors.white,
-              ),
+        appBar: AppBar(
+          title: Text(
+            _title != null ? _title! : 'Chat App',
+            style: TextStyle(
+              color: Colors.white,
             ),
-            centerTitle: true,
           ),
-          body: _appScreen == 'Loading'
-              ? Center(
-                  child: Text('Loading...'),
-                )
-              : _appScreen == 'Glowbom'
-                  ? Center(
-                      child:
-                          const Image(image: AssetImage('assets/glowbom.png')),
-                    )
-                  : ChatScreen(
-                      _content != null && _content.containsKey('start_over')
-                          ? _content['start_over']
-                          : 'AI',
-                      _questions,
-                      _voice!,
-                    )),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.file_upload),
+              onPressed: _startFilePicker,
+            ),
+          ],
+        ),
+        body: _appScreen == 'Loading'
+            ? Center(
+                child: Text('Loading...'),
+              )
+            : _appScreen == 'Glowbom'
+                ? Center(
+                    child: const Image(image: AssetImage('assets/glowbom.png')),
+                  )
+                : ChatScreen(
+                    _content != null && _content.containsKey('start_over')
+                        ? _content['start_over']
+                        : 'AI',
+                    _questions,
+                    _voice!,
+                  ),
+      ),
     );
   }
 }
