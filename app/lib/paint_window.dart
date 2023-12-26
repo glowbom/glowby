7379ce58b6d6c2e2091ae6375885e7a0536ec658
Jacob <jacob@glowbom.com>
2023-12-26 15:22:06 -0800

    feat: add conversion of drawing to base64 in PaintWindow

--- app/lib/paint_window.dart
+++ app/lib/paint_window.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'dart:typed_data';
 
 class PaintWindow extends StatefulWidget {
   @override
@@ -17,13 +21,50 @@ class _PaintWindowState extends State<PaintWindow> {
     super.dispose();
   }
 
+  // This function converts the drawing (list of points) to a base64 string
+  Future<String> convertToBase64(List<Offset?> points) async {
+    // Create a picture recorder to record the canvas operations
+    final ui.PictureRecorder recorder = ui.PictureRecorder();
+    final Canvas canvas = Canvas(recorder);
+
+    // Draw your points here onto the canvas
+    final paint = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2.0;
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i]!, points[i + 1]!, paint);
+      }
+    }
+
+    // End recording the canvas operations
+    final ui.Picture picture = recorder.endRecording();
+
+    // Convert the picture to an image
+    final ui.Image image =
+        await picture.toImage(300, 300); // Set the width and height as needed
+    final ByteData? byteData =
+        await image.toByteData(format: ui.ImageByteFormat.png);
+
+    // Convert the byte data to a Uint8List
+    final Uint8List imgBytes = byteData!.buffer.asUint8List();
+
+    // Base64 encode the image bytes
+    final String base64String = base64Encode(imgBytes);
+
+    return base64String;
+  }
+
   Future<void> callOpenAI() async {
     setState(() {
       isLoading = true;
     });
     // Convert points to a suitable format and call OpenAI method
     // For example, you might convert points to an image and then to base64
-    // String imageBase64 = convertToBase64(points); // Implement this function
+    String imageBase64 =
+        await convertToBase64(points); // Implement this function
+    print(imageBase64);
     // String htmlResponse = await OpenAI_API().getHtmlFromOpenAI(imageBase64, creationName);
 
     // Do something with htmlResponse
