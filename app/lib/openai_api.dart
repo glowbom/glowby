edc5eb2227f8d4300dcaca7bfab06f505f8d0bd9
Jacob <jacob@glowbom.com>
2023-12-26 22:30:15 -0800

    feat: enter api key prompt

--- app/lib/openai_api.dart
+++ app/lib/openai_api.dart
@@ -445,6 +445,10 @@ class OpenAI_API {
 
   Future<String> getHtmlFromOpenAI(
       String imageBase64, String userPrompt) async {
+    if (_apiKey == '') {
+      return 'Enter API key in settings';
+    }
+
     final systemPrompt = """
 You are a skilled web developer with expertise in Tailwind CSS. A user will provide a low-fidelity wireframe along with descriptive notes. Your task is to create a high-fidelity, responsive HTML webpage using Tailwind CSS and JavaScript, embedded within a single HTML file.
 
