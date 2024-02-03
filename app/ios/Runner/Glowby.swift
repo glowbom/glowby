bd8c4b113c3cb991daeb89164f43ebe530ceea5f
Jacob <jacob@glowbom.com>
2024-02-02 19:48:47 -0800

    feat: the first version of AR Glowby

--- app/ios/Runner/Glowby.swift
+++ app/ios/Runner/Glowby.swift
@@ -9,17 +9,26 @@ import UIKit
 import RealityKit
 
 class GlowbyARView: UIView {
-
     var arView: ARView!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        arView = ARView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
+        arView = ARView(frame: .zero) // Initialize with zero frame
+        arView.autoresizingMask = [.flexibleWidth, .flexibleHeight] // Use autoresizing instead
         self.addSubview(arView)
-        // Setup the rest of your AR scene here.
+
+        // Setup RealityKit scene here
+        let anchor = AnchorEntity(world: .zero) // Corrected to use the right initializer
+        let mesh = MeshResource.generateBox(size: 0.1) // Creates a cube of 10cm x 10cm x 10cm
+        let material = SimpleMaterial(color: .blue, isMetallic: true)
+        let cube = ModelEntity(mesh: mesh, materials: [material])
+        anchor.addChild(cube)
+
+        arView.scene.anchors.append(anchor)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
